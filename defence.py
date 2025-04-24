a0fbdad3c80bb8558287a846e20711153801d811
Andre <andrea.alberti708@gmail.com>
2025-04-24 19:28:29 +0200

    s

--- defence.py
+++ defence.py
@@ -58,6 +58,10 @@ def save_images(images, filename, save_dir):
     os.makedirs(save_dir, exist_ok=True)
     filename = filename.replace(".", ",")
     for i, img_array in enumerate(images):
+        img_array = np.transpose(img_array, (1, 2, 0)) 
+        # Se float, scala a 0-255 e converti in uint8
+        if img_array.dtype == np.float32 or img_array.max() <= 1.0:
+            img_array = (img_array * 255).clip(0, 255).astype(np.uint8)
         img = Image.fromarray(img_array)
         img.save(os.path.join(save_dir, filename + f'_{i}.jpg'), 'JPEG')
 
@@ -72,7 +76,7 @@ def generate_adversarial_train_set(classifier, x_test):
     n_samples = x_test.shape[0]
     adv_examples = []
 
-    attack_types = {"fgsm", "bim", "pgd", "df", "cw"}
+    attack_types = ["fgsm", "bim", "pgd", "df", "cw"]
     for attack in attack_types:
 
         if attack in ["fgsm", "bim", "pgd", "df"]:
@@ -85,32 +89,32 @@ def generate_adversarial_train_set(classifier, x_test):
                 start, end = i * split_size, (i + 1) * split_size if i < len(epsilon_values) - 1 else n_samples
                 x_subset = x_test[start:end]
                 attack = FastGradientMethod(estimator=classifier, eps=eps, targeted=targeted)
-                adv_examples.append(attack.generate(x=x_subset))
-                save_images(adv_examples[-1], f"fgsm_eps_{eps}", save_dir)
+                adv_examples = attack.generate(x=x_subset)
+                save_images(adv_examples, f"eps_{eps}", save_dir + "/fgsm")
 
         elif attack == "bim":
             for i, eps in enumerate(epsilon_values):
                 start, end = i * split_size, (i + 1) * split_size if i < len(epsilon_values) - 1 else n_samples
                 x_subset = x_test[start:end]
                 attack = BasicIterativeMethod(estimator=classifier, eps=eps, eps_step=0.005, max_iter=10)
-                adv_examples.append(attack.generate(x=x_subset))
-                save_images(adv_examples[-1], f"bim_eps_{eps}", save_dir)
+                adv_examples = attack.generate(x=x_subset)
+                save_images(adv_examples, f"eps_{eps}", save_dir + "/bim")
 
         elif attack == "pgd":
             for i, eps in enumerate(epsilon_values):
                 start, end = i * split_size, (i + 1) * split_size if i < len(epsilon_values) - 1 else n_samples
                 x_subset = x_test[start:end]
                 attack = ProjectedGradientDescent(estimator=classifier, eps=eps, eps_step=0.005, max_iter=10)
-                adv_examples.append(attack.generate(x=x_subset))
-                save_images(adv_examples[-1], f"pgd_eps_{eps}", save_dir)
+                adv_examples = attack.generate(x=x_subset)
+                save_images(adv_examples, f"eps_{eps}", save_dir + "/pgd")
 
         elif attack == "df":
             for i, eps in enumerate(epsilon_values):
                 start, end = i * split_size, (i + 1) * split_size if i < len(epsilon_values) - 1 else n_samples
                 x_subset = x_test[start:end]
                 attack = DeepFool(classifier=classifier, epsilon = eps, max_iter=5, batch_size=16)
-                adv_examples.append(attack.generate(x=x_subset))
-                save_images(adv_examples[-1], f"df_eps_{eps}", save_dir)
+                adv_examples = attack.generate(x=x_subset)
+                save_images(adv_examples, f"eps_{eps}", save_dir + "/df")
 
         elif attack == "cw":
             for i, conf in enumerate(confidence_values):
@@ -123,16 +127,32 @@ def generate_adversarial_train_set(classifier, x_test):
                     learning_rate=0.01,
                     batch_size=8
                 )
-                adv_examples.append(attack.generate(x=x_subset))
-                save_images(adv_examples[-1], f"cw_confidence_{conf}", save_dir)
+                adv_examples = attack.generate(x=x_subset)
+                save_images(adv_examples, f"confidence_{conf}", save_dir + "/cw")
 
     x_test_adv = np.concatenate(adv_examples, axis=0)
     return x_test_adv
 
 
+def get_train_set_attack(attack_type):
+    path = os.path.join("./dataset/detectors_train_set", attack_type)
+    image_list = []
+
+    for filename in sorted(os.listdir(path)):
+        if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+            img_path = os.path.join(path, filename)
+            image = Image.open(img_path).convert("RGB")
+            image = np.array(image, dtype=np.uint8)         # (H, W, C), uint8
+            image = np.transpose(image, (2, 0, 1))           # → (C, H, W)
+            image_list.append(image)
+
+    return np.stack(image_list, axis=0)
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_detectors', type=bool, default=False, help='Se True, addestra i detector; altrimenti carica i modelli salvati')
+    parser.add_argument('--generate_train', type=bool, default=False, help='Se True, genera il train adv set; altrimenti no')
+    parser.add_argument('--train_detectors', type=bool, default=True, help='Se True, addestra i detector; altrimenti carica i modelli salvati e procede con la valutazione')
     parser.add_argument('--threshold', type=float, default=0.5, help='Threshold per le rilevazioni dei detector')
     parser.add_argument("--attack", type=str, default="bim", choices=["fgsm", "bim", "pgd", "df", "cw"], help="Type of attack to test")
     parser.add_argument("--targeted", type=bool, default=False, help="Test on targeted attack")
@@ -142,45 +162,36 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
+    # Directory per i modelli
+    os.makedirs("./models", exist_ok=True)
+
     # Setup dei classificatori
     classifierNN1= setup_classifier(device)
 
-    # Carica le immagini e le etichette del test set
-    test_set = get_test_set()
-    test_images, test_labels = test_set.get_images()
-
-    # Carica le immagini e le etichette del training set
-    if args.train_detectors:
+    # Generazione del training set avversario
+    if args.generate_train:
         train_set = get_train_set()
         train_images = train_set.get_images()
-        # Divisione in train e test set 80% - 20%
-        #train_images, _, test_images, test_labels = train_test_split(test_images, test_labels, test_size=0.2)  # per fare prove veloci (800 img invece che 4k).
-
-    # Calcolo dell'accuracy sulle immagini clean rispetto alle label vere
-    accuracy_nn1_clean = compute_accuracy(classifierNN1, test_images, test_labels)
-    print(f"Accuracy del classificatore NN1 su dati clean: {accuracy_nn1_clean}")
-
-    # Directory per i modelli
-    os.makedirs("./models", exist_ok=True)
-    #attack_types = {"fgsm", "bim", "pgd", "df", "cw"}
-    attack_types = {"fgsm", "bim", "pgd"}
-    #attack_types = {"bim"}
+        attack_types = ["fgsm", "bim", "pgd", "df", "cw"]
+        generate_adversarial_train_set(classifierNN1, train_images, attack_types)
 
     # Train or load Detectors
     detectors = {}
     nb_train = train_images.shape[0]
-    for attack_type in attack_types:
-        model_path = os.path.join("./models", f"{attack_type}_detector.pth")
-        detector_classifier = setup_detector_classifier(device)
-        if args.train_detectors:
+    #attack_types = ["fgsm", "bim", "pgd", "df", "cw"]
+    attack_types = ["fgsm"]
+    # Fase di train dei detector
+    if args.train_detectors:
+
+        for attack_type in attack_types:
+            model_path = os.path.join("./models", f"{attack_type}_detector.pth")
+            detector_classifier = setup_detector_classifier(device)
+
             print(f"Training detector for attack: {attack_type}")
             detectors[attack_type] = BinaryInputDetector(detector_classifier)
-            if attack_type == "df":
-                classifier = setup_classifier(device, classify=False)
-            else:
-                classifier = setup_classifier(device, classify=True)
+            
             # Train the detector
-            x_train_adv = generate_adversarial_examples(classifier, attack_type, train_images)
+            x_train_adv = get_train_set_attack(attack_type)
             x_train_detector = np.concatenate((train_images, x_train_adv), axis=0)
             y_train_detector = np.concatenate((np.array([[1, 0]] * nb_train), np.array([[0, 1]] * nb_train)), axis=0)
             detectors[attack_type].fit(x_train_detector, y_train_detector, nb_epochs=20, batch_size=16, verbose=True)
@@ -188,41 +199,54 @@ def main():
             # Salvataggio dello state_dict del modello
             torch.save(detector_classifier.model.state_dict(), model_path)
             print(f"Detector salvato in: {model_path}")
-        else:
-            print(f"Caricamento del detector per attack: {attack_type}")
+    
+    # Carica i detector dai modelli salvati
+    else:
+        for attack_type in attack_types:
+            model_path = os.path.join("./models", f"{attack_type}_detector.pth")
+            detector_classifier = setup_detector_classifier(device)
             detector_classifier.model.load_state_dict(torch.load(model_path, map_location=device))
             detector_classifier.model.eval()
             detectors[attack_type] = BinaryInputDetector(detector_classifier)
             print(f"Detector caricato da: {model_path}")
 
-    # Valutare detectors + classifier sui dati clean del test set
-    nb_test = test_images.shape[0]
-    adv_labels = np.zeros(nb_test, dtype=bool) # Tutti i campioni sono puliti (classe 0)
-    accuracy_clean, fp = compute_accuracy_with_detectors(classifierNN1, test_images, test_labels, adv_labels, detectors, threshold=args.threshold, verbose=True)
-    print(f"Accuracy del classificatore col filtraggio dei detectors: {accuracy_clean:.4f}")
-    print(f"Numero di immagini scartate dai detectors (FP): {fp}")
+        #### FASE DI VALUTAZIONE ####
+        # Carica le immagini e le etichette del test set
+        test_set = get_test_set()
+        test_images, test_labels = test_set.get_images()
 
-    # Calcolo della targeted accuracy sulle immagini clean rispetto alle label della classe target
-    target_class_label = "Cristiano_Ronaldo"
-    target_class = test_set.get_true_label(target_class_label)
-    targeted_labels = target_class * torch.ones(test_labels.size, dtype=torch.long)
-    targeted_accuracy_clean, fp = compute_accuracy_with_detectors(classifierNN1, test_images, targeted_labels, adv_labels, detectors, threshold=args.threshold, verbose=True)
-    print(f"Accuracy del classificatore col filtraggio dei detectors: {targeted_accuracy_clean:.4f}")
-    print(f"Numero di immagini scartate dai detectors (FP): {fp}")
+        # Calcolo dell'accuracy sulle immagini clean rispetto alle label vere
+        accuracy_nn1_clean = compute_accuracy(classifierNN1, test_images, test_labels)
+        print(f"Accuracy del classificatore NN1 su dati clean: {accuracy_nn1_clean}")
 
-    # Valutare detectors + classifier sui dati adversarial
-    # Avvio dell'attacco selezionato
-    if args.attack == "fgsm":
-        run_fgsm(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
-    elif args.attack == "bim":
-        run_bim(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
-    elif args.attack == "pgd":
-        run_pgd(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
-    elif args.attack == "df":
-        classifierNN1 = setup_classifier(device, classify=False)
-        run_df(classifierNN1, None, test_images, test_labels, accuracy_clean, None, detectors, args.threshold)
-    elif args.attack == "cw":
-        run_cw(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
+        # Valutare detectors + classifier sui dati clean del test set
+        nb_test = test_images.shape[0]
+        adv_labels = np.zeros(nb_test, dtype=bool) # Tutti i campioni sono puliti (classe 0)
+        accuracy_clean, fp = compute_accuracy_with_detectors(classifierNN1, test_images, test_labels, adv_labels, detectors, threshold=args.threshold, verbose=True)
+        print(f"Accuracy del classificatore col filtraggio dei detectors: {accuracy_clean:.4f}")
+        print(f"Numero di immagini scartate dai detectors (FP): {fp}")
+
+        # Calcolo della targeted accuracy sulle immagini clean rispetto alle label della classe target
+        target_class_label = "Cristiano_Ronaldo"
+        target_class = test_set.get_true_label(target_class_label)
+        targeted_labels = target_class * torch.ones(test_labels.size, dtype=torch.long)
+        targeted_accuracy_clean, fp = compute_accuracy_with_detectors(classifierNN1, test_images, targeted_labels, adv_labels, detectors, threshold=args.threshold, verbose=True)
+        print(f"Accuracy del classificatore col filtraggio dei detectors: {targeted_accuracy_clean:.4f}")
+        print(f"Numero di immagini scartate dai detectors (FP): {fp}")
+
+        # Valutare detectors + classifier sui dati adversarial
+        # Avvio dell'attacco selezionato
+        if args.attack == "fgsm":
+            run_fgsm(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
+        elif args.attack == "bim":
+            run_bim(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
+        elif args.attack == "pgd":
+            run_pgd(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
+        elif args.attack == "df":
+            classifierNN1 = setup_classifier(device, classify=False)
+            run_df(classifierNN1, None, test_images, test_labels, accuracy_clean, None, detectors, args.threshold)
+        elif args.attack == "cw":
+            run_cw(classifierNN1, None, test_images, test_labels, accuracy_clean, None, args.targeted, targeted_accuracy_clean, None, target_class, detectors, args.threshold)
 
 
 if __name__ == "__main__":
